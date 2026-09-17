7d43414df4dfc285306fb233d302ca5792d084fc
NangRotha <rothanang21@gmail.com>
2026-09-17 17:21:15 +0700

    fix domain

--- app/main.py
+++ app/main.py
@@ -120,7 +120,7 @@ app = FastAPI(title="E-commerce API")
 _DEV_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:5174",
-    "http://localhost:5175",
+    "https://www.udomkh.online",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:5174",
     "http://127.0.0.1:5175",
